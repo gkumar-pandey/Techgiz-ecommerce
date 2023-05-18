81d31888bb1d8fc4c703ed3fc0b4249992eeee06
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-18 12:46:01 +0530

    refact: add common styles

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -18,19 +18,22 @@ const Navbar = () => {
 
   return (
     <>
-      <nav>
-        <div className="logo_container">
-          <IoMenu className="menu_icon" onClick={HandleSideNavbar} />
-          <h3 className="nav-logo">TechGiz</h3>
+      <nav className="d-flex items-center justify-between ">
+        <div className=" d-flex items-center logo_container">
+          <IoMenu
+            className="d-none menu_icon cursor-pointer "
+            onClick={HandleSideNavbar}
+          />
+          <h3 className="nav-logo cursor-pointer ">TechGiz</h3>
         </div>
-        <div className="search_bar">
-          <button>
+        <div className=" d-flex items-center search_bar">
+          <button className="d-flex items-center justify-center">
             <BiSearch className="search_bar_icon" />
           </button>
           <input className="search_input" type="text" placeholder="Search..." />
         </div>
         <div>
-          <ul className="nav_links">
+          <ul className="d-flex">
             <li className="nav_link profile-icon">
               <AiOutlineUser className="nav_link_icon" />
               <span className="nav_link_icon_title">Login</span>
