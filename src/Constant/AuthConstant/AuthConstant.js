a2dbe87d701a35761529a5ea49e667f61da6919e
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-19 13:04:37 +0530

    feat: constants created

---
+++ src/Constant/AuthConstant/AuthConstant.js
@@ -0,0 +1,8 @@
+export const SET_EMAIL = "SET_EMAIL";
+export const SET_PASSWORD = "SET_PASSWORD";
+export const SET_FIRST_NAME = "SET_FIRST_NAME";
+export const SET_LAST_NAME = "SET_LAST_NAME";
+export const SET_CONFIRM_PASSWORD = "SET_CONFIRM_PASSWORD";
+export const SET_IS_SAVE_USER = "SET_IS_SAVE_USER";
+export const SET_LOADING = "SET_LOADING";
+export const SAVE_USER = "SAVE_USER";
