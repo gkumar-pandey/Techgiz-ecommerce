c292c340f7d87ecc856335bb6bb54f2170cbf034
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-24 20:43:45 +0530

    products page context provider added

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./Context/AuthContext/AuthContext";
+import { ProductsProvider } from "./Context";
 
 // Call make Server
 makeServer();
@@ -13,7 +14,9 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>,
