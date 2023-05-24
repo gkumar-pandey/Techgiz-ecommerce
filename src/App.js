be60a77293c8ea35272c57b0c97f56e3a8569cdc
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-24 20:43:17 +0530

    products page route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import { Navbar } from "./Components";
 import { LoginPage, Signuppage } from "./Pages";
+import ProductsPage from "./Pages/Productspage/ProductsPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<Signuppage />} />
+        <Route path="/products" element={<ProductsPage />} />
       </Routes>
     </div>
   );
