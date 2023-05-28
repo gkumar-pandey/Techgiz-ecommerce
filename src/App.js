ffb7c091784e5962069f2a46ae0a312e80acd87e
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-28 08:34:30 +0530

    feat: added toaster

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import { Navbar } from "./Components";
-import { LoginPage, Signuppage } from "./Pages";
+
+import { Toaster } from "react-hot-toast";
+import SignUpPage from "./Pages/Authpages/SignUpPage/SignUpPage";
+import LoginPage from "./Pages/Authpages/LoginPage/LoginPage";
 
 function App() {
   return (
     <div>
+      <Toaster position="top-center" reverseOrder={false} />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<Signuppage />} />
+        <Route path="/signup" element={<SignUpPage />} />
       </Routes>
     </div>
   );
