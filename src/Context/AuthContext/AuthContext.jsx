55e80863fa036a90cff2dcf4d1df72d15cfbc950
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-28 08:36:16 +0530

    feat: implement "signUpHandler" & "loginHandler" function for login and signup which takes user details and validate inputs value and sent to backend

--- src/Context/AuthContext/AuthContext.jsx
+++ src/Context/AuthContext/AuthContext.jsx
@@ -1,5 +1,9 @@
-import { createContext, useContext, useEffect, useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 import { authReducer } from "../../Reducer";
+
+import { LOADING, SET_LOGIN_ERROR, SET_SIGNUP_ERROR } from "../../Constant";
+import { loginService, signupService } from "../../Services";
+import { loginFromValidation, signUpFormValidation } from "../../Utils";
 
 const AuthContext = createContext();
 
@@ -11,15 +15,53 @@ const userInitialState = {
     password: "",
     confirmPassword: ""
   },
+  loginUser: {
+    email: "",
+    password: ""
+  },
   isSaveUser: false,
-  isLoading: false
+  isLoading: false,
+  signUpError: "",
+  loginError: ""
 };
 
 export const AuthProvider = ({ children }) => {
   const [userState, dispatchUser] = useReducer(authReducer, userInitialState);
 
+  const signupHandler = async (e) => {
+    e.preventDefault();
+    dispatchUser({ type: LOADING, payload: true });
+
+    const isValid = signUpFormValidation(userState, dispatchUser);
+
+    if (isValid) {
+      dispatchUser({ type: SET_SIGNUP_ERROR, payload: "" });
+      await signupService(userState.user, dispatchUser);
+      dispatchUser({ type: LOADING, payload: false });
+    } else {
+      dispatchUser({ type: LOADING, payload: false });
+    }
+  };
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    dispatchUser({ type: LOADING, payload: true });
+
+    const isValid = loginFromValidation(userState.loginUser, dispatchUser);
+
+    if (isValid) {
+      dispatchUser({ type: SET_LOGIN_ERROR, payload: "" });
+      await loginService(userState.loginUser, dispatchUser);
+      dispatchUser({ type: LOADING, payload: false });
+    } else {
+      dispatchUser({ type: LOADING, payload: false });
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ userState, dispatchUser }}>
+    <AuthContext.Provider
+      value={{ userState, dispatchUser, signupHandler, loginHandler }}
+    >
       {children}
     </AuthContext.Provider>
   );
