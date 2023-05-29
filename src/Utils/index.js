9c1cdc311bef56d843c1a5fdfbf26321bcfbc095
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-29 11:32:59 +0530

    feat: filter functionality completed

---
+++ src/Utils/index.js
@@ -0,0 +1,6 @@
+export * from "./filters/getFilterDataByCategory";
+export * from "./filters/getFilterDataByRating";
+export * from "./filters/getFilterDataBySearch";
+export * from "./filters/getFilterDataByPriceRange";
+export * from "./filters/getSortedData";
+export * from "./filters/getFilterDataByStock";
