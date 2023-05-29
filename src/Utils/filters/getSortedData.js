0795d08a6fd931921651562906357fbaebd6d11a
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-29 11:28:26 +0530

    feat: "getSortedData" function takes products data and sort type and filter data according to price lowtohign or hightolow or latest product

---
+++ src/Utils/filters/getSortedData.js
@@ -0,0 +1,15 @@
+export const getSortedData = (products, sortBy) => {
+  if (sortBy === "LOW_TO_HIGH") {
+    return products.sort(
+      (a, b) =>
+        parseInt(a.price.replace(",", "")) - parseInt(b.price.replace(",", ""))
+    );
+  } else if (sortBy === "HIGH_TO_LOW") {
+    return products.sort(
+      (a, b) =>
+        parseInt(b.price.replace(",", "")) - parseInt(a.price.replace(",", ""))
+    );
+  } else if (sortBy === "LATEST") {
+    return products.sort((a, b) => b.addedInYear - a.addedInYear);
+  }
+};
