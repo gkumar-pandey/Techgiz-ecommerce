a3ea58dd03f57094f9058a4038ee29cf7ffa90b2
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-29 16:10:08 +0530

    wishlist route added

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import { Navbar } from "./Components";
-import { LoginPage, Signuppage } from "./Pages";
+import { LoginPage, Signuppage, WishlistPage } from "./Pages";
 import ProductsPage from "./Pages/Productspage/ProductsPage";
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<Signuppage />} />
         <Route path="/products" element={<ProductsPage />} />
+        <Route path="/wishlist" element={<WishlistPage />} />
       </Routes>
     </div>
   );
