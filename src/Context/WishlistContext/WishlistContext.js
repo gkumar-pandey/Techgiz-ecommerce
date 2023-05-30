65ae9c8ed24eeeb2ef193ca81692cb3e819469ec
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-30 11:42:49 +0530

    feat: add product to wishlist or remove product to wishlist function implemented

--- src/Context/WishlistContext/WishlistContext.js
+++ src/Context/WishlistContext/WishlistContext.js
@@ -1,21 +1,59 @@
-import { createContext, useContext, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import { WishlistReducer } from "../../Reducer/WishlistReducer/WishlistReducer";
+import { addToWishlist, removeFromWishlist } from "../../Services";
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const WishlistContext = createContext();
 
 const wishlistInitialState = {
-  products: []
+  products: [],
+  isLoading: false
 };
 export const WishlistProvider = ({ children }) => {
   const [wishlistProductState, dispatchWishlist] = useReducer(
     WishlistReducer,
     wishlistInitialState
   );
+  const navigate = useNavigate();
 
-  console.log(wishlistProductState.products);
+  const isUserLoggedIn = JSON.parse(localStorage.getItem("user"))?.token;
+
+  const addToWishlistHandler = (product) => {
+    if (isUserLoggedIn) {
+      const res = addToWishlist(product, dispatchWishlist);
+      toast.promise(res, {
+        loading: "Loading...",
+        success: `${product.productName} added to wishlist`,
+        error: "Something went wrong!!"
+      });
+    } else {
+      navigate("/login");
+    }
+  };
+
+  const removeFromWishlistHandler = (productId, productName) => {
+    if (isUserLoggedIn) {
+      const res = removeFromWishlist(productId, dispatchWishlist);
+      toast.promise(res, {
+        loading: "Loading...",
+        success: `${productName} removed from wishlist`,
+        error: "Something went wrong!!"
+      });
+    } else {
+      navigate("/login");
+    }
+  };
+
   return (
     <WishlistContext.Provider
-      value={{ dispatchWishlist, wishlistProductState }}
+      value={{
+        dispatchWishlist,
+        wishlistProductState,
+        isUserLoggedIn,
+        addToWishlistHandler,
+        removeFromWishlistHandler
+      }}
     >
       {children}
     </WishlistContext.Provider>
