1c1e22a91daf05db2b1fbcac7df6c8d082e61262
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-30 11:45:12 +0530

    exported wishlist services funtions

--- src/Services/index.js
+++ src/Services/index.js
@@ -3,3 +3,5 @@ export * from "./authServices/signUpService.js";
 export * from "./categoryServices/getCategory";
 export * from "./productsServices/fetchAllProducts";
 export * from "./wishlistServices/getWishlistProducts";
+export * from "./wishlistServices/addToWishlist.js";
+export * from "./wishlistServices/removeFromWishlist.js";
