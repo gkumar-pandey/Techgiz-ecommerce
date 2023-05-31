ff69e631c112bdaff43fae247af7be484769ccf2
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-31 17:29:50 +0530

    feat: UI of cart page created

---
+++ src/Pages/CartPage/Components/CartEmpty.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CartEmpty = () => {
+  return (
+    <div>
+      <div>
+        <h2>Your Cart is empty</h2>
+        <p>Explore more products </p>
+      </div>
+    </div>
+  );
+};
+
+export default CartEmpty;
