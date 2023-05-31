13585a766eede55753a76b32c3a2c1d4fab97414
gkumar-pandey <gautamshekhar078@gmail.com>
2023-06-01 00:30:54 +0530

    feat: fetch cart data and render on page if empty show empty msg

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,23 +1,53 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./CartPage.css";
-import HorizontalProductCard from "../../Components/ProductCard/HorizontalProductCard";
 import PriceDetails from "./Components/PriceDetails";
+import { useCart } from "../../Context/CartContext/CartContext";
+import { Loader, EmptyMsgComp, HorizontalProductCard } from "../../Components";
+import { getCartProducts } from "../../Services";
 
 const CartPage = () => {
+  const {
+    dispatchCart,
+    cartState: { products, isLoading },
+    isUserLoggedIn
+  } = useCart();
+  useEffect(() => {
+    if (isUserLoggedIn) {
+      getCartProducts(dispatchCart);
+    }
+  }, []);
   return (
-    <div className=" cart_container   ">
-      <div className="cart_heading">
-        <h2>Shopping Cart (4) </h2>
-      </div>
-      <div className="cart w-full ">
-        <div className="cart_products mx-1 ">
-          <HorizontalProductCard />
-        </div>
-        <div className=" cart_details  ">
-          <PriceDetails />
-        </div>
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <>
+          {products.length > 0 ? (
+            <>
+              <div className=" cart_container">
+                <div className="cart_heading">
+                  <h2>Shopping Cart ({products.length}) </h2>
+                </div>
+                <div className="cart w-full ">
+                  <div className="cart_products mx-1 ">
+                    {products.map((product, idx) => (
+                      <HorizontalProductCard {...product} key={product._id} />
+                    ))}
+                  </div>
+                  <div className=" cart_details  ">
+                    <PriceDetails />
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <EmptyMsgComp msg={"Your cart is empty."} />
+            </>
+          )}
+        </>
+      )}
+    </>
   );
 };
 
--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,6 +1,7 @@
 import WishlistPage from "./WishlistPage/WishlistPage";
+import CartPage from "./CartPage/CartPage";
 export * from "./Home/Home";
 export * from "./Authpages/index";
 export * from "./Productspage/ProductsPage";
 
-export { WishlistPage };
+export { WishlistPage, CartPage };
