25f3cb2c5d799f45e6a92399f2a318b6d380c15c
gkumar-pandey <gautamshekhar078@gmail.com>
2023-05-31 23:55:39 +0530

    feat: loader created

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,19 +1,17 @@
 import React from "react";
-import { ThreeDots } from "react-loader-spinner";
+import { ColorRing } from "react-loader-spinner";
 
 const Loader = () => {
   return (
     <>
-      <ThreeDots
+      <ColorRing
+        visible={true}
         height="80"
         width="80"
-        radius="9"
-        color="#fff"
         ariaLabel="blocks-loading"
         wrapperStyle={{}}
         wrapperClass="blocks-wrapper"
         colors={["#302f34"]}
-        visible={true}
       />
     </>
   );
