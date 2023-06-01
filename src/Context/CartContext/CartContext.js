45aa8be8f82f0466764ec5081b3171ad44e98779
gkumar-pandey <gautamshekhar078@gmail.com>
2023-06-01 11:16:36 +0530

    feat: "addToCartHandler"=> takes product as input check user is logged in if yes then call addToCart service and make a post req and show alter if not looged in navitage to login page

--- src/Context/CartContext/CartContext.js
+++ src/Context/CartContext/CartContext.js
@@ -1,5 +1,8 @@
 import { createContext, useContext, useReducer } from "react";
 import { CartReducer } from "../../Reducer/CartReducer/CartReducer";
+import { addToCart } from "../../Services";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const CartContext = createContext();
 const initialCartState = {
@@ -8,9 +11,31 @@ const initialCartState = {
 };
 export const CartContextProvider = ({ children }) => {
   const [cartState, dispatchCart] = useReducer(CartReducer, initialCartState);
+  const navigate = useNavigate();
   const isUserLoggedIn = JSON.parse(localStorage.getItem("user"))?.token;
+
+  const addToCartHandler = (product) => {
+    if (isUserLoggedIn) {
+      const res = addToCart(product, dispatchCart);
+      toast.promise(res, {
+        loading: "Loading..",
+        success: `${product.productName} added to cart `,
+        error: "Something went wrong!!"
+      });
+    } else {
+      navigate("/login");
+    }
+  };
   return (
-    <CartContext.Provider value={{ dispatchCart, isUserLoggedIn, cartState }}>
+    <CartContext.Provider
+      value={{
+        dispatchCart,
+        isUserLoggedIn,
+        cartState,
+        addToCart,
+        addToCartHandler
+      }}
+    >
       {children}
     </CartContext.Provider>
   );
--- src/Context/index.js
+++ src/Context/index.js
@@ -1,5 +1,6 @@
 import { WishlistProvider } from "./WishlistContext/WishlistContext.js";
 export * from "./AuthContext/AuthContext.jsx";
 export * from "./ProductsContext/ProductsContext.jsx";
+export * from "./CartContext/CartContext.js";
 
 export { WishlistProvider };
