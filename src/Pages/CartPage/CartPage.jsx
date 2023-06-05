3241591bb5e5165ccc9b6afae91e84d4a0de0655
gkumar-pandey <gautamshekhar078@gmail.com>
2023-06-05 18:02:42 +0530

    feat: cart page completed

--- src/Pages/CartPage/CartPage.jsx
+++ src/Pages/CartPage/CartPage.jsx
@@ -1,11 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./CartPage.css";
 import PriceDetails from "./Components/PriceDetails";
 import { useCart } from "../../Context/CartContext/CartContext";
 import { Loader, EmptyMsgComp, HorizontalProductCard } from "../../Components";
 import { getCartProducts } from "../../Services";
+import Modal from "../../Components/Modal/Modal";
+import DiscountComp from "./Components/DiscountComp";
+import { totalPriceOfCartProducts } from "../../Utils/cart-details/totalPriceOfCartProducts";
 
 const CartPage = () => {
+  const [isApplyDiscount, setIsApplyDiscount] = useState(false);
+  const [discountCoupon, setDiscountCoupon] = useState([]);
   const {
     dispatchCart,
     cartState: { products, isLoading },
@@ -16,6 +21,33 @@ const CartPage = () => {
       getCartProducts(dispatchCart);
     }
   }, []);
+
+  const handleModal = () => {
+    setIsApplyDiscount(!isApplyDiscount);
+  };
+
+  const handleDiscountCoupon = (e) => {
+    if (e.target.checked) {
+      setDiscountCoupon((pre) => [...pre, e.target.value]);
+    } else {
+      setDiscountCoupon((pre) => pre.filter((item) => item !== e.target.value));
+    }
+  };
+
+  const totalPrice = totalPriceOfCartProducts(products);
+  const totalProductQty = products.reduce((acc, curr) => acc + curr.qty, 0);
+  const deliveryCharges = totalPrice > 1000 ? "Free" : "₹40";
+  const totalAmount = totalPrice + (totalPrice < 1000 && 40);
+  const discountPrice = discountCoupon.reduce(
+    (acc, curr) =>
+      curr === "OFF100"
+        ? acc + 100
+        : curr === "10%NEWUSER"
+        ? acc + totalAmount * 0.1
+        : acc,
+    0
+  );
+
   return (
     <>
       {isLoading ? (
@@ -35,10 +67,26 @@ const CartPage = () => {
                     ))}
                   </div>
                   <div className=" cart_details  ">
-                    <PriceDetails />
+                    <PriceDetails
+                      totalPrice={totalPrice}
+                      totalAmount={totalAmount}
+                      totalProductQty={totalProductQty}
+                      deliveryCharges={deliveryCharges}
+                      discountPrice={discountPrice}
+                      handleModal={handleModal}
+                    />
                   </div>
                 </div>
               </div>
+              {isApplyDiscount && (
+                <Modal>
+                  <DiscountComp
+                    handleDiscountCoupon={handleDiscountCoupon}
+                    handleModal={handleModal}
+                    discountCoupon={discountCoupon}
+                  />
+                </Modal>
+              )}
             </>
           ) : (
             <>
