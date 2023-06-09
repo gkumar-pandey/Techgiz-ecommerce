18e1fb2640a5925cc8b9c5938187e73baaf86814
gkumar-pandey <gautamshekhar078@gmail.com>
2023-06-10 03:17:36 +0530

    fix: show products without filter when click on shop btn

--- src/Pages/Home/Components/Banner.jsx
+++ src/Pages/Home/Components/Banner.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import { bannerImgLink } from "../../../StaticData";
 import "./Banner.css";
 import { Link } from "react-router-dom";
+import { RESET_FILTER } from "../../../Constant/FilterConstant/FilterConstant";
+import { useProducts } from "../../../Context";
 
 const Banner = () => {
+  const { dispatchFilter } = useProducts();
   return (
     <section>
       <div className="relative">
         <img src={bannerImgLink} className="banner_img" alt="banner-img" />
-        <Link to={"/products"}>
+        <Link
+          to={"/products"}
+          onClick={() => dispatchFilter({ type: RESET_FILTER })}
+        >
           <button className="shop_now_btn absolute cursor-pointer ">
             Shop now
           </button>
